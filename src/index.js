c3180bcec3bf7b5ff59ec53fda59bbdb664b50c9
meforoma <meforoma@gmail.com>
2023-01-05 09:15:41 +0200

    04 Setup CORS policy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import { authRouter } from './routers/authRouter.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(authRouter);
 
